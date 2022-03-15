bc8b42e3e46987942351a2e210ff18257eb2813a
tommybebe <hansupanda@gmail.com>
2022-03-15 18:13:01 +0900

    feat: add metric generator

---
+++ src/metric_generator.py
@@ -0,0 +1,65 @@
+import yaml
+from string import Template
+from yaml.loader import SafeLoader
+
+
+def yaml_to_dict(file_path: str) -> dict:
+    with open(file_path, "r") as file: 
+        config = yaml.load(file, Loader=SafeLoader)
+        return config["metrics"]
+
+
+class ModelMaker:
+    def __init__(self, config: dict, metric_desitination: str):
+        self.name = config["name"]
+        self.time_grains = config["time_grains"]
+        self.metric_desitination = metric_desitination
+    
+    def __make_query(self, grain: str) -> str:
+        query_template = Template('''{{ config(materialized="table") }}
+        select * 
+        from {{ metrics.metric(
+            metric_name="$name",
+            grain="$grain",
+            secondary_calculations=[
+                metrics.period_over_period(comparison_strategy="ratio", interval=1),
+                metrics.period_over_period(comparison_strategy="difference", interval=1),
+                metrics.period_to_date(aggregate="average", period="month"),
+                metrics.period_to_date(aggregate="sum", period="year"),
+                metrics.rolling(aggregate="average", interval=4),
+                metrics.rolling(aggregate="min", interval=4)
+            ]
+        ) }}
+        ''')
+        query = query_template.substitute(name=self.name, grain=grain)
+        return query
+
+    def __get_model_prefix(self, grain: str) -> str:
+        grains = {
+            "day": "daily",
+            "week": "weekly",
+            "month": "monthly"
+        }
+        return f"metric__{grains[grain]}"
+
+    def __make_model(self, grain) -> None:
+        query = self.__make_query(grain)
+        prefix = self.__get_model_prefix(grain)
+        model_name = f"{prefix}_{self.name}"
+        with open(f"{self.metric_desitination}/{model_name}.sql", "w") as file:
+            file.write(query)
+    
+    def make_models(self):
+        for grain in self.time_grains:
+            self.__make_model(grain)
+
+
+if __name__ == "__main__":
+    file_path = "./models/mart/schema.yml"
+    metric_desitination = "./models/metric"
+
+    configs = yaml_to_dict(file_path)
+
+    for config in configs: 
+        model_maker = ModelMaker(config, metric_desitination)
+        model_maker.make_models()
